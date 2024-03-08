16cb1127417f56081340406ff4ef12bf9b9dcd43
wardatuljannah16 <160722195+wardatuljannah16@users.noreply.github.com>
2024-03-08 11:24:51 +0700

    Add files via upload

---
+++ genZ.py
@@ -0,0 +1,7 @@
+tahun_lahir = int (input("masukan tahun lahir anda:"))
+if tahun_lahir >= 2000:
+    hasil = "wadawww anda merupakan genZ"
+else:
+    hasil = "anda bukan genZ ningg"
+
+print(hasil)
